1f3a4f97d13539250c9f3e1a78c09b0688c30556
Robert Kumanek <robert.kumanek@gmail.com>
2022-01-09 22:07:11 +0100

    Add StdPlayerInput
    Issue: #7

--- inc/PlayerInput.hpp
+++ inc/PlayerInput.hpp
@@ -8,3 +8,8 @@ class PlayerInputI {
   virtual ~PlayerInputI() {}
   virtual std::tuple<char, int, int> makeMove() = 0;
 };
+
+class StdPlayerInput : public PlayerInputI {
+ public:
+  std::tuple<char, int, int> makeMove() override;
+};
